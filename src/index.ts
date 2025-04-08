6d3b0e6253180a182636578e6cff6fa391196a3d
AhmedSaIah <ahmedsalah5446@gmail.com>
2025-04-08 14:25:35 +0200

    all: products

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express, { Request, Response } from 'express';
+import { ProductController } from './interfaces/controllers/product-controller';
+import { ProductRepository } from './domain/repositories/product-repository';
+import { ProductRepositoryI } from './infrastructure/repositories/product-repository-i';
+import { PrismaClient } from '@prisma/client';
+import ProductRouter from './interfaces/routes/product-router';
+
+const db = new PrismaClient();
+const productRepository: ProductRepository = new ProductRepositoryI(db);
+const productController = new ProductController(productRepository);
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Routes
 app.get('/', (_: Request, res: Response) => {
-  res.send({message: 'Server is running! 🚀'});
+  res.json({ message: 'Store API is running! 🚀' });
 });
+
+app.use('/api/v1/products', new ProductRouter(productController).getRouter());
 
 app.listen(PORT, () => {
   console.log(`API is running on port ${PORT}`);
